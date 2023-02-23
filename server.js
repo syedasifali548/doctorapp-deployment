3f7eb505a17180fb183d230a323f1e9a818d23a9
Syed Asif Ali <57398037+syedasifali548@users.noreply.github.com>
2023-02-23 06:11:46 -0800

    final-code

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const colors = require("colors");
 const moragan = require("morgan");
 const connectDB = require("./config/db");
 const cors = require("cors");
-
+const path = require('path')
 const port = 5000 || process.env.PORT;
 
 
@@ -15,10 +15,16 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(moragan("dev"));
 app.use(cors());
+// Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/admin", require("./routes/adminRoutes"));
 app.use("/api/doctor", require("./routes/doctorRoutes"));
 
+// Static files
+app.use(express.static(path.join(__dirname,'./client/build')))
+app.get('*',function(req,res){
+  res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
 
 app.listen(port, () => {
   // console.log(
